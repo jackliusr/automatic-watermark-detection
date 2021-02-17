26a039b8b7e672dd6fae1b04d578b890f168392d
Jack Liu S. R <jackliusr@gmail.com>
2021-02-17 21:20:41 +0800

    tf migration from tf 1 to tf2

--- src/tensorflow_experiments.py
+++ src/tensorflow_experiments.py
@@ -5,10 +5,9 @@ import os
 import scipy
 from scipy.sparse import *
 
-tf.compat.v1.disable_v2_behavior()
 # helpers that are going to be useful here
 sobel_x = tf.constant([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], tf.float32)
-sobel_y = tf.transpose(sobel_x)
+sobel_y = tf.transpose(a=sobel_x)
 
 sobel_x_filter = tf.stack([sobel_x, sobel_x, sobel_x])
 sobel_x_filter = tf.stack([sobel_x_filter, sobel_x_filter, sobel_x_filter])
@@ -23,26 +22,26 @@ def phi_func(mtensor, epsilon=0.001):
 def E_data(I, W, J, alpha):
     est_error = tf.multiply(alpha, W) + tf.multiply(1-alpha, I) - J
     est_error = phi_func(tf.square(est_error))
-    est_error = tf.reduce_mean(est_error)
+    est_error = tf.reduce_mean(input_tensor=est_error)
     return est_error
 
 # regularizer term for I, W
 def E_reg(I, alpha):
     alpha_ = tf.expand_dims(alpha, 0)
-    ax = tf.nn.conv2d(alpha_, sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME")
-    ay = tf.nn.conv2d(alpha_, sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME")
-    Ix2 = tf.square(tf.nn.conv2d(I, sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME"))
-    Iy2 = tf.square(tf.nn.conv2d(I, sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME"))
+    ax = tf.nn.conv2d(input=alpha_, filters=sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME")
+    ay = tf.nn.conv2d(input=alpha_, filters=sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME")
+    Ix2 = tf.square(tf.nn.conv2d(input=I, filters=sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME"))
+    Iy2 = tf.square(tf.nn.conv2d(input=I, filters=sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME"))
     est_error = tf.multiply(tf.abs(ax), Ix2) + tf.multiply(tf.abs(ay), Iy2)
-    est_error = tf.reduce_mean(phi_func(est_error))
+    est_error = tf.reduce_mean(input_tensor=phi_func(est_error))
     return est_error
 
 # regularization term for alpha
 def E_reg_alpha(alpha):
     alpha_ = tf.expand_dims(alpha, 0)
-    ax2 = tf.square(tf.nn.conv2d(alpha_, sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME"))
-    ay2 = tf.square(tf.nn.conv2d(alpha_, sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME"))
-    est_error = tf.reduce_mean(phi_func(ax2 + ay2))
+    ax2 = tf.square(tf.nn.conv2d(input=alpha_, filters=sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME"))
+    ay2 = tf.square(tf.nn.conv2d(input=alpha_, filters=sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME"))
+    est_error = tf.reduce_mean(input_tensor=phi_func(ax2 + ay2))
     return est_error
 
 # fidelity term
@@ -53,21 +52,21 @@ def E_f(alpha, W, W_m):
     if len(shape) == 3:
         aW = tf.expand_dims(aW, 0)
     # find edge map of alpha*W
-    aWx = tf.nn.conv2d(aW, sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME")
-    aWy = tf.nn.conv2d(aW, sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME")
+    aWx = tf.nn.conv2d(input=aW, filters=sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME")
+    aWy = tf.nn.conv2d(input=aW, filters=sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME")
     aW_ = tf.sqrt(tf.square(aWx) + tf.square(aWy))
     
     # find edge map of W_m
     W_m__ = tf.expand_dims(W_m, 0)
-    W_mx = tf.nn.conv2d(W_m__, sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME")
-    W_my = tf.nn.conv2d(W_m__, sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME")
+    W_mx = tf.nn.conv2d(input=W_m__, filters=sobel_x_filter, strides=[1, 1, 1, 1], padding="SAME")
+    W_my = tf.nn.conv2d(input=W_m__, filters=sobel_y_filter, strides=[1, 1, 1, 1], padding="SAME")
     W_m_ = tf.sqrt(tf.square(W_mx) + tf.square(W_my))
     
-    return tf.reduce_mean(phi_func(tf.square(aW_ - W_m_)))
+    return tf.reduce_mean(input_tensor=phi_func(tf.square(aW_ - W_m_)))
 
 # auxiliary term
 def E_aux(W, W_k):
-    return tf.reduce_mean(tf.abs(W - W_k))
+    return tf.reduce_mean(input_tensor=tf.abs(W - W_k))
 
 # We try to use Tensorflow to perform the 3 steps
 def image_watermark_decompose_model(num_images, m, n, chan=3, l_i=1, l_w=1, l_alpha=1, beta=1, gamma=1, lr=0.07):
@@ -84,10 +83,10 @@ def image_watermark_decompose_model(num_images, m, n, chan=3, l_i=1, l_w=1, l_al
     #    W(k) = (num_images, m, n, chan) -> all watermarks
     
     # All placeholders
-    J = tf.Variable(tf.float32, shape=(num_images, m, n, chan), name='J')
-    alpha = tf.Variable(tf.float32, shape=(m, n, chan), name='alpha')
-    W_m = tf.Variable(tf.float32, shape=(m, n, chan), name='W_m')
-    W_median = tf.Variable(tf.float32, shape=(m, n, chan), name='W_median')
+    J = tf.compat.v1.placeholder(tf.float32, shape=(num_images, m, n, chan), name='J')
+    alpha = tf.compat.v1.placeholder(tf.float32, shape=(m, n, chan), name='alpha')
+    W_m = tf.compat.v1.placeholder(tf.float32, shape=(m, n, chan), name='W_m')
+    W_median = tf.compat.v1.placeholder(tf.float32, shape=(m, n, chan), name='W_median')
     
     # All variables
     I = tf.Variable(np.random.randn(num_images, m, n, chan), name='I', dtype=tf.float32)
@@ -97,7 +96,7 @@ def image_watermark_decompose_model(num_images, m, n, chan=3, l_i=1, l_w=1, l_al
     loss = E_data(I, W, J, alpha) + l_i*E_reg(I, alpha) + l_w*E_reg(W, alpha) \
             + beta*E_f(alpha, W, W_m) + gamma*E_aux(W_median, W)
     
-    optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
+    optimizer = tf.compat.v1.train.GradientDescentOptimizer(lr).minimize(loss)
     return {
         'J': J,
         'alpha': alpha,
@@ -115,16 +114,16 @@ def matte_update_model(num_images, m, n, chan=3, l_alpha=1, beta=1, lr=0.07):
     # We use the rest of the items as constants and only estimate alpha
 
     # All placeholders
-    J = tf.Variable(tf.float32, shape=(num_images, m, n, chan), name='J')
-    W_m = tf.Variable(tf.float32, shape=(m, n, chan), name='W_m')
-    W_median = tf.Variable(tf.float32, shape=(m, n, chan), name='W_median')
-    I = tf.Variable(tf.float32, shape=(num_images, m, n, chan), name='I')
-    W = tf.Variable(tf.float32, shape=(num_images, m, n, chan), name='W')
+    J = tf.compat.v1.placeholder(tf.float32, shape=(num_images, m, n, chan), name='J')
+    W_m = tf.compat.v1.placeholder(tf.float32, shape=(m, n, chan), name='W_m')
+    W_median = tf.compat.v1.placeholder(tf.float32, shape=(m, n, chan), name='W_median')
+    I = tf.compat.v1.placeholder(tf.float32, shape=(num_images, m, n, chan), name='I')
+    W = tf.compat.v1.placeholder(tf.float32, shape=(num_images, m, n, chan), name='W')
 
     alpha = tf.Variable(np.random.randn(m, n, chan), dtype=tf.float32)
 
     loss = E_data(I, W, J, alpha) + l_alpha*E_reg_alpha(alpha) + beta*E_f(alpha, W_median, W_m)
-    optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
+    optimizer = tf.compat.v1.train.GradientDescentOptimizer(lr).minimize(loss)
 
     return {
         'J': J,
